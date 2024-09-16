4bb19b10e9d90b41270fd263fec7b469791779fe
KNIGHTCORE47 <knightcoreashifrealmex2pro@gmail.com>
2024-09-16 17:22:47 +0530

    Initialization of NextJs SAAS application powerd by Cloudinary, installation of cloudinary, daisyUI, clerk, usage of ClerkProvider and modification of layout page with it, creation of middleware, design logic in middleware for user authententication, usage of clerkMiddleware, and createRouteMatcher, define environment variables.

---
+++ src/middleware.ts
@@ -0,0 +1,48 @@
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+import { NextResponse } from 'next/server';
+
+const isPublicRoute = createRouteMatcher([
+    "/sign-in",
+    "/sign-up",
+    "/",
+    "/home",
+])
+
+const isPublicApiRoute = createRouteMatcher([
+    "/api/videos",
+])
+
+export default clerkMiddleware(( auth, req) => {
+    const {userId} = auth();
+    const currentUrl = new URL(req.url)
+    const isAccessingDashboard = currentUrl.pathname === "/home"
+
+    const isApiRequest = currentUrl.pathname.startsWith("/api")
+
+//NOTE - This is only for public routes, Here we are checking if user is logged in, want to access the public routes but not want to access dashboard then redirect user to dashboard homepage
+    if (userId && isPublicRoute(req) && !isAccessingDashboard) {
+        return NextResponse.redirect(new URL("/home", req.url))
+    }
+
+    //NOTE - User not logged in 
+
+    if (!userId) {
+        //NOTE - User not logged in and want to access public routes(Protected Routes)
+        if (!isPublicRoute(req) && !isPublicApiRoute(req)) {
+            return NextResponse.redirect(new URL("/sign-in", req.url))
+        }
+
+        //NOTE - User not logged in and request for accessing API(Protected API)
+        if (isApiRequest && !isPublicApiRoute(req)) {
+            return NextResponse.redirect(new URL("/sign-in", req.url))
+            
+        }
+    }
+
+    return NextResponse.next()
+
+})
+
+export const config = {
+    matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+  }
